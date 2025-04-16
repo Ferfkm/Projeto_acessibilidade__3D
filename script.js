345182dacc494bcc3e07f795ceae2b0ca24e73f4
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2025-04-16 10:42:43 +0000

    ok

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function(){
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte +=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
-
+    
     diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
